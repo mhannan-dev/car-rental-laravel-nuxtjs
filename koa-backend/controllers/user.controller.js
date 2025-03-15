e2daeadb9fb62af5b777ecd0bb2b504e3f73d112
Muhammad Hannan <mdhannan.info@gmail.com>
2025-03-16 05:43:21 +0600

    .shopify

--- koa-backend/controllers/user.controller.js
+++ koa-backend/controllers/user.controller.js
@@ -1,9 +1,8 @@
 exports.getUsers = async (ctx) => {
-    ctx.body = { users: ['User1', 'User2', 'User3'] };
-  };
-  
-  exports.getUser = async (ctx) => {
-    const { id } = ctx.params;
-    ctx.body = { user: `User ${id}` };
-  };
-  
+  ctx.body = { users: ['User1', 'User2', 'User3'] };
+};
+
+exports.getUser = async (ctx) => {
+  const { id } = ctx.params;
+  ctx.body = { user: `User ${id}` };
+};
